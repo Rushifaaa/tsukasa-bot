401ce1373acdbb1e06bbdfed7701679c5235247f
Rushifaaa <jakubgencer@protonmail.com>
2019-12-02 16:10:18 +0100

    Added play command not good yet and
    created a configfile

--- src/commands/commands.ts
+++ src/commands/commands.ts
@@ -1,9 +1,13 @@
 import * as Discord from 'discord.js';
+import { } from 'ytdl-core';
+import ytdl = require('ytdl-core');
 
 export default class commands {
 
     msg: Discord.Message;
     prefix: string = "†";
+    queue = new Map();
+    serverQueue = null
 
     constructor(msg: Discord.Message, client: Discord.Client) {
         this.msg = msg;
@@ -12,6 +16,7 @@ export default class commands {
     public commands() {
         this.ping();
         this.voiceChannel();
+        this.play();
     }
 
     private ping() {
@@ -45,5 +50,84 @@ export default class commands {
                 this.msg.reply('You need to be in the same channel!');
             }
         }
+    }
+
+    private async play() {
+        this.serverQueue = this.queue.get(this.msg.guild.id);
+
+        var args = this.msg.content.slice(this.prefix.length).trim().split(/ +/s);
+        var command = args.shift().toLowerCase();
+
+        if (command === "play") {
+            console.log("###ARGS[0] -> ", args[0])
+            if (!args[0]) return this.msg.reply("Please enter a vailid link!");
+
+            const argsLink = this.msg.content.split(" ");
+
+            const voiceChannel = this.msg.member.voiceChannel;
+
+            if (!voiceChannel) return this.msg.reply("You need to be in a Channel!");
+            const permission = voiceChannel.permissionsFor(this.msg.client.user);
+
+            if (!permission.has("CONNECT") && !permission.has("SPEAK")) {
+                return this.msg.channel.send("I need permission to Join and Speak in a Channel!");
+            }
+
+            const songInfo = await ytdl.getInfo(argsLink[1]);
+            const song = {
+                title: songInfo.title,
+                url: songInfo.video_url
+            }
+
+            if (!this.serverQueue) {
+                const queueConstruct = {
+                    textChannel: this.msg.channel,
+                    voiceChannel: voiceChannel,
+                    connection: null,
+                    songs: [],
+                    volume: 5,
+                    playing: true,
+                };
+                this.queue.set(this.msg.guild.id, queueConstruct);
+
+                queueConstruct.songs.push(song);
+
+
+                try {
+                    var connection = await voiceChannel.join();
+                    queueConstruct.connection = connection;
+                    //TODO: Play song!
+
+                    this.playSong(this.msg.guild, queueConstruct.songs[0]);
+                } catch (error) {
+                    console.log(error);
+                    this.queue.delete(this.msg.guild.id);
+                    return this.msg.channel.send("There was an error playing -> " + "`" + error + "`");
+                }
+            } else {
+                this.serverQueue.songs.push(song);
+                return this.msg.channel.send(`${song.title} has been added successfully to the queue. :heart:`);
+            }
+        }
+    }
+
+    private playSong(guild: Discord.Guild, song: any) {
+        const serverQueue = this.queue.get(this.msg.guild.id);
+
+        if (!song) {
+            serverQueue.voiceChannel.leave();
+            this.queue.delete(this.msg.guild.id);
+            return;
+        }
+
+        const dispatcher = serverQueue.connection.playStream(ytdl(song.url))
+            .on('end', () => {
+                serverQueue.songs.shift();
+                this.playSong(guild, serverQueue.songs[0]);
+            })
+            .on('error', error => {
+                console.log(error);
+            });
+        dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
     }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,35 +1,62 @@
 import { Client, Role } from 'discord.js';
 import commands from './commands/commands';
+import { readdirSync, readdir, writeFile, readFile, statSync, existsSync, writeFileSync, readFileSync } from 'fs';
 
 require('dotenv').config();
 
 const tsukasa = new Client();
+const configFilePath = __dirname + "/../config.json";
 
+interface TsukasaConfig {
+    token: string;
+}
 
 tsukasa.on('ready', () => {
     console.log(`Logged in as ${tsukasa.user.tag}`);
 
+    //TODO: Make json file for every server
+
     tsukasa.user.setPresence({
         status: "dnd",
         afk: true,
-		game: {
-			name: "how lucifer creates me",
-			url: "https://github.com/Rushifaaa/tsukasa-bot",
-			type: "WATCHING"
-		}
+        game: {
+            name: "how lucifer creates me",
+            url: "https://github.com/Rushifaaa/tsukasa-bot",
+            type: "WATCHING"
+        }
     });
 });
 
 tsukasa.on('guildMemberAdd', member => {
-   member.addRole("508733818547601447").then(() => {
-       console.log("Added Role to user" + member.displayName);
-   }).catch(err => {
-       console.log("An error occurred! -> " + err)
-   });
+    member.addRole("508733818547601447").then(() => {
+        console.log("Added Role to user" + member.displayName);
+    }).catch(err => {
+        console.log("An error occurred! -> " + err)
+    });
 });
 
 tsukasa.on('message', msg => {
     new commands(msg, tsukasa).commands()
 });
 
-tsukasa.login(process.env.TOKEN);
+async function startServer() {
+    var newConfig: TsukasaConfig = {
+        token: "TOKEN"
+    };
+
+    if (!existsSync(configFilePath)) {
+        writeFileSync(configFilePath, JSON.stringify(newConfig));
+        console.log("Config file was created, please set up!")
+        return;
+    }
+
+    try {
+        const config: TsukasaConfig = JSON.parse(readFileSync(configFilePath).toString());
+        tsukasa.login(config.token);
+    } catch (error) {
+        console.log("Fix config file please, could be empty?", error);
+    }
+}
+
+startServer()
+
