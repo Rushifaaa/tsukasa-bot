f0a1ee1ad3268367e4f49739246a5ce7f7e4a615
Rushifaaa <jakubgencer@protonmail.com>
2019-12-04 11:55:00 +0100

    pause/resume now works

--- src/command.ts
+++ src/command.ts
@@ -7,15 +7,17 @@ import quit from "./commands/voice-channel/quit";
 import play from "./commands/music/play";
 import { GuildData } from "./main";
 import stop from "./commands/music/stop";
+import pause from "./commands/music/pause";
+import resume from "./commands/music/resume";
 
 export class Command {
 
-    constructor(readonly name: string, readonly aliases: string[], readonly func: (args: string[], msg: Message, guildObject: Map<string, GuildData>) => number | Promise<any> | void) {
+    constructor(readonly name: string, readonly aliases: string[], readonly func: (args: string[], msg: Message, guildData: Map<string, GuildData>) => number | Promise<any> | void) {
         //EMPTY....
     }
 
-    invoke(args: string[], msg: Message, guildObject: Map<string, GuildData>): number {
-        const ret = this.func(args, msg, guildObject);
+    invoke(args: string[], msg: Message, guildData: Map<string, GuildData>): number {
+        const ret = this.func(args, msg, guildData);
         if (typeof (ret) !== "number") return 0;
         return ret;
     }
@@ -25,6 +27,8 @@ export const commands = [
     new Command("ping", [], ping),
     new Command("join", [], join),
     new Command("play", [], play),
+    new Command("pause", [], pause),
+    new Command("resume", [], resume),
     new Command("stop", [], stop),
     new Command("disconnect", ["dc"], disconnect),
     new Command("git", ["dev", "developer"], git),
