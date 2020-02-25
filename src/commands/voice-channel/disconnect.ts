04ac1edd87fd040d19b6e5839dbabaf6c7af2cd2
Rushifaaa <jakubgencer@protonmail.com>
2020-02-25 14:01:04 +0100

    disconnect file refactored, added comments

--- src/commands/voice-channel/disconnect.ts
+++ src/commands/voice-channel/disconnect.ts
@@ -1,25 +1,36 @@
 import { Message } from 'discord.js';
 import { GuildData } from '../../main';
 
-const disconnect = (args: string[], msg: Message, guildObjects: Map<string, GuildData>) => {
+const disconnect = (args: string[], msg: Message, guildObjects: Map<string, GuildData>): void => {
 
+    // Checking if the channel type is dm 
     if (msg.channel.type === "dm") {
         msg.channel.send("Is just available on a Server!");
         return;
     }
 
+    // Getting the guild
     const guild = guildObjects.get(msg.guild.id);
 
+    // Checking if the bot is in a voicechannel or has a voice connection
     if (msg.member.voiceChannel || msg.guild.voiceConnection) {
+        // Checking if the user is in the same channel as the bot
         if (msg.member.voiceChannelID === msg.guild.me.voiceChannelID) {
 
+            // Checking if guld is available
             if (!guild) {
                 msg.reply("Guild not found!");
                 return;
             }
 
+            
             msg.reply("see you, my darling!! :p :heart:");
-
+            /**
+             * Setting the isStoped boolean to true
+             * Setting the songs array to an empty array
+             * Setting the dispatcher to null
+             * Leaving the channel
+             */
             guild.isStoped = true;
             guild.songs = [];
             guild.dispatcher = null;
@@ -30,5 +41,5 @@ const disconnect = (args: string[], msg: Message, guildObjects: Map<string, Guil
     } else {
         msg.reply("I need to be in a channel for that!");
     }
-}
+};
 export default disconnect;
