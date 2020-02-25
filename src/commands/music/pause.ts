21bb8a40a6db9c6bfda60301dc5280b2dae03597
Rushifaaa <jakubgencer@protonmail.com>
2020-02-25 12:59:20 +0100

    pause file refactored, added comments

--- src/commands/music/pause.ts
+++ src/commands/music/pause.ts
@@ -1,31 +1,38 @@
 import { Message } from 'discord.js';
 import { GuildData } from '../../main';
 
-const pause = (args: string[], msg: Message, guildData: Map<string, GuildData>) => {
-
+const pause = (args: string[], msg: Message, guildData: Map<string, GuildData>): void => {
+    
+    // Checking if the channel type is dm
     if (msg.channel.type === "dm") {
         msg.channel.send("Is just available on a Server!");
         return;
     }
 
+    // Getting the guild
     const guild = guildData.get(msg.guild.id);
 
+    // Checking if guild is null
     if (!guild) {
         msg.reply("Server not found!");
         return;
     }
 
+    // Checking if guild dispatcher is null
     if (!guild.dispatcher) {
         msg.reply("No Stream is running.");
         return;
     }
 
+    // Checking if audio stream is paused
     if (guild.dispatcher.paused) {
         msg.reply("Stream is already paused, darling :heart:");
         return;
     }
 
+    // Pausing the audio stream
     guild.dispatcher.pause();
     msg.reply("I paused the stream for you, :heart:");
-}
+
+};
 export default pause;
