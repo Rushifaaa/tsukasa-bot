574bf1f0736b896e8a0eaa7fd7e2c5b69840746a
Rushifaaa <jakubgencer@protonmail.com>
2020-02-25 12:54:16 +0100

    role file refactored, added comments

--- src/commands/moderation/role.ts
+++ src/commands/moderation/role.ts
@@ -7,22 +7,28 @@ enum Subcommands {
     Remove = "remove",
 }
 
-const role = (args: string[], msg: Message): void => {
+const role = async (args: string[], msg: Message): Promise<void> => {
+    // Checking if the channel type is dm
     if (msg.channel.type === "dm") {
         msg.channel.send("You can't use this command in a dm!");
         return;
     }
 
+    // Checking the permission with the "permissionCheck" function
     if (!permissionCheck(msg)) {
         msg.channel.send("You don't have the permission to perform this command!");
         return;
     }
 
+    // Checking the args[0] if it equals the subcommands
     if (args[0] == Subcommands.List) {
+
+        // Listing all available roles of the guild
         msg.channel.send("I'll list all available roles on this guild...");
         const roles: Collection<string, Role> = msg.guild.roles;
 
         const rolesName: string[] = [];
+        
         roles.forEach(role => {
             rolesName.push(role.name);
         });
@@ -34,5 +40,5 @@ const role = (args: string[], msg: Message): void => {
     } else if (args[0] == Subcommands.Remove) {
         // TODO: DO SOMETHING
     }
-}
+};
 export default role;
