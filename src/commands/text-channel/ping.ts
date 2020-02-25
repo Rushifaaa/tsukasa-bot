5e2c371030ebcb48e54a001cf557ee58e583cbcd
Rushifaaa <jakubgencer@protonmail.com>
2020-02-25 13:52:21 +0100

    added void to return type

--- src/commands/text-channel/ping.ts
+++ src/commands/text-channel/ping.ts
@@ -1,7 +1,6 @@
 import { Message } from 'discord.js';
 
-const ping = (args: string[], msg: Message) => {
+const ping = (args: string[], msg: Message): void => {
     msg.channel.send("pong :heart:");
-    //msg.author.send("pong :heart: -> PM");
-}
+};
 export default ping;
