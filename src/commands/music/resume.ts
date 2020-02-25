a96602e117206e869caf56d82339a6dd14ff41d6
Rushifaaa <jakubgencer@protonmail.com>
2020-02-25 13:34:06 +0100

    resume file refactored, added comments

--- src/commands/music/resume.ts
+++ src/commands/music/resume.ts
@@ -1,25 +1,29 @@
 import { Message } from 'discord.js';
 import { GuildData } from '../../main';
 
-const resume = (args: string[], msg: Message, guildData: Map<string, GuildData>) => {
-
+const resume = (args: string[], msg: Message, guildData: Map<string, GuildData>): void => {
+    // Checking if channel type is dm
     if (msg.channel.type === "dm") {
         msg.channel.send("Is just available on a Server!");
         return;
     }
 
+    // Getting the guild
     const guild = guildData.get(msg.guild.id);
 
+    // Checking if guild is available
     if (!guild) {
         msg.reply("Server not found!");
         return;
     }
 
+    // Checking if playstream is null
     if (!guild.dispatcher) {
         msg.reply("No Stream is running.");
         return;
     }
 
+    // Checking if playstream is paused
     if (guild.dispatcher.paused) {
         guild.dispatcher.resume();
         msg.reply("Resuming the stream :heart:");
@@ -27,5 +31,5 @@ const resume = (args: string[], msg: Message, guildData: Map<string, GuildData>)
     }
 
     msg.reply("Nothing to resume, darling :heart:");
-}
+};
 export default resume;
